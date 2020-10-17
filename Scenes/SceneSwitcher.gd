7c7293020bb3309f95365fed1ed4391c3de78554
dustinc555 <dckook10@gmail.com>
2020-10-17 01:16:14 -0500

    Made portal not a tile

--- Scenes/SceneSwitcher.gd
+++ Scenes/SceneSwitcher.gd
@@ -12,17 +12,21 @@ var rng = RandomNumberGenerator.new()
 
 var room_resources =  [ResourceLoader.load("res://Scenes/Rooms/EmptyRoom.tscn")]
 
-# [dx, dy] : int tuple
-func travel(dist):
+func travel(dx: int, dy: int, player: Object):
+	print('Travel Event: [%d, %d]' % [location_x + dx, location_y + dy])
 	# if at edge just return
-	var scene_instance = room_matrix[location_x + dist[0]][location_y + dist[1]]
-	call_deferred("_deferred_goto_scene", scene_instance)
+	var scene_instance = room_matrix[location_x + dx][location_y + dy]
+	call_deferred("_deferred_goto_scene", scene_instance, player)
 
 
-func _deferred_goto_scene(scene_instance):
+func _deferred_goto_scene(scene_instance, player):
+	# TODO Player retains location and inf loops enters a new room, 
+	# set player moved state 
+	# TODO have player enter next room at corresponding portal
+	get_tree().get_root().get_node('Dungeon/Walls').remove_child(player)
 	get_tree().get_root().add_child(scene_instance)
 	get_tree().set_current_scene(scene_instance)
-
+	get_tree().get_root().get_node('Dungeon/Walls').add_child(player)
 
 func randomize_matrix(x, y):
 	if (x < 0 || x > len(room_matrix) - 1 
@@ -53,3 +57,9 @@ func _ready():
 	room_matrix[0][0] = room_resources[rng.randi_range(0, len(room_resources) - 1)].instance()
 	randomize_matrix(1, 0)
 	randomize_matrix(0, 1)
+	
+	var player = ResourceLoader.load("res://troll.tscn").instance()
+	# TODO: move player creation logic
+	get_tree().get_root().get_node('Dungeon/Walls').add_child(player)
+	
+	travel(location_x, location_y, player)
